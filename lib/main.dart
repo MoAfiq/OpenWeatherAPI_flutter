e99cdccf441fcf44f0d3f9828322da5bec8e41b5
Afiq Salleh <afiqsalleh97@gmail.com>
2022-08-05 11:04:43 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter WeatherMap API'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Please enter city name: '),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(padding: EdgeInsets.all(15)),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'City',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            ElevatedButton(
+              child: Text('Seatch'),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
